02b5c1f32e63319239a1288752c5de7073a5a728
Alexandre Abadie <alexandre.abadie@inria.fr>
2016-10-27 17:56:39 +0200

    script/iotlab-demo: improve launcher script and document the demo

---
+++ scripts/README.md
@@ -0,0 +1,45 @@
+## Demos scripts
+
+Here one can find scripts to automatize the deployment of demonstrations. This
+README explains how to previously setup your environment and how to run the
+script.
+
+### IoT-LAB A8-M3 demo
+
+This demonstration is provided by `iotlab-launch-demo` and will book 2 nodes in
+the IoT-LAB testbed in Saclay site. One of the node is configured as a boarder
+router, the other one is running the demonstration firmware `node_iotlab_a8_m3`.
+
+You need an IoT-LAB account to run this script.
+
+#### Requirements
+
+1. Install IoT-LAB cli-tools and ssh-cli-tools packages (you have to be part of
+   the IoT-LAB organisation to access the later one, which is private for the
+   moment):
+```shell
+$ git clone git@github.com:iot-lab/ssh-cli-tools.git
+$ cd ssh-cli-tools
+$ pip install -e . --user
+$ cd ..
+$ git clone git@github.com:iot-lab/cli-tools.git
+$ cd cli-tools
+$ pip install -e . --user
+$ cd ..
+```
+2. Play at least once the [RIOT border router tutorial on IoT-LAB
+   portal](https://www.iot-lab.info/tutorials/riot-public-ipv66lowpan-network-with-a8-m3-nodes/)
+   Important: use the same directories on the Saclay site ssh frontend
+3. Clone this repository:
+```shell
+$ git clone git@gitlab.inria.fr:fit-saclay/demos.git
+```
+4. Launch the demo on nodes A8 144 (the border router) and A8 145 (the demo
+   node).
+```shell
+$ ./iotlab-launch-demo.sh 144 ./gnrc_border_router.elf 145 ./dashboard_riot_a8_m3.elf
+```
+5. You should now see the demo node on the [fit-demo live
+   camera](demo-fit.saclay.inria.fr) and play its LED from the [RIOT
+   dashboard](fit-demo.saclay.inria.fr/dashboard)
+
--- scripts/iotlab-launch-demo.sh
+++ scripts/iotlab-launch-demo.sh
@@ -1,23 +1,85 @@
 #!/bin/bash
 
-BR_FW = $1
-DEMO_FW = $2
+BR_A8_ID=$1
+BR_FW=$2
 
-# Submit an experiment in Saclay on nodes A8 144 and 145
-# - node-a8-144 will be used as a border router
-# - node-a8-145 will run the dashboard enable firmware. This node is visible at
-# http://demo-fit.saclay.inria.fr
-experiment-cli submit -d 120 -l saclay,a8,144+145
-experiment-cli wait
+DEMO_A8_ID=$3
+DEMO_FW=$4
 
-# Flash the firmwares on the nodes using ssh-cli-tools
-# 1. Flash the border router
-open-a8-cli update-m3 $(BR_FW) -l saclay,a8,144
+ETHOS_DIR="~/A8/riot/RIOT/dist/tools/ethos"
+SSH_MAX_TRIES=30
 
-# Start the border router
-ssh -t node-a8-144 'cd ~/A8/riot/RIOT/dist/tools/ethos&& screen -S br ./start_network.sh /dev/ttyA8_M3 tap0 2001:0660:3207:490::/64'
+start_experiment() {
+    # Submit an experiment in Saclay on nodes A8 144 and 145
+    # - node-a8-144 will be used as a border router
+    # - node-a8-145 will run the dashboard enable firmware. This node is visible at
+    # http://demo-fit.saclay.inria.fr
+    echo "Starting new experiment"
+    experiment-cli submit -d 120 -l saclay,a8,${BR_A8_ID}+${DEMO_A8_ID}
+    experiment-cli wait
+    echo "Experiment started"
+}
 
-# 2. Flash the demo node
-open-a8-cli update-m3 $(DEMO_FW) -l saclay,a8,145
+check_ssh_available() {
+    for node_id in ${BR_A8_ID} ${DEMO_A8_ID}
+    do
+        zero=0
+        cpt=${SSH_MAX_TRIES}
+        while ((cpt))
+        do
+            echo -ne "\e[0K\rNode ${node_id}: ${cpt} remaining ssh tries   "
+            ssh -q node-a8-${node_id} exit > /dev/null 2>&1
+            if [[ $? -eq $zero ]]
+            then
+                echo -e
+                echo "SSH available on node-a8-${node_id}"
+                break
+            fi
+            ((cpt--))
+            sleep 2s
+        done
+        if [[ $cpt -eq $zero ]]
+        then
+            echo -e
+            echo "Could not connect to one of the nodes, exiting"
+            exit 1
+        fi
+    done
+}
 
+start_border_router() {
+    # Flash the firmwares on the nodes using ssh-cli-tools
+    # 1. Flash the border router
+    echo "Flashing border router firmware"
+    open-a8-cli update-m3 ${BR_FW} -l saclay,a8,${BR_A8_ID} > iotlab-launch-demo.log
 
+    echo "Configuring the border router"
+    # Configure RIOT on the border router
+    ssh node-a8-${BR_A8_ID} "cd ${ETHOS_DIR}/../uhcpd && make clean all" > /dev/null
+    ssh node-a8-${BR_A8_ID} "cd ${ETHOS_DIR} && make clean all" > /dev/null
+
+    # Stop any running screen
+    screen -X -S br quit
+
+    # Start the border router
+    PREFIX_LINE=`ssh node-a8-144 "source /etc/profile.d/ipv6 && printenv | grep INET6_PREFIX="`
+    PREFIX_LIST=(${PREFIX_LINE/\=/ })
+    PREFIX=${PREFIX_LIST[${#PREFIX_LIST[@]} - 1]}::/64
+    screen -S br -dm ssh -t node-a8-${BR_A8_ID} "cd ${ETHOS_DIR} && ./start_network.sh /dev/ttyA8_M3 tap0 ${PREFIX}"
+    echo "Border router started"
+}
+
+start_demo_node() {
+    # 2. Flash the demo node
+    open-a8-cli update-m3 ${DEMO_FW} -l saclay,a8,${DEMO_A8_ID} >> iotlab-launch-demo.log
+}
+
+stop_demo() {
+    echo "Exiting"
+    trap "" INT QUIT TERM EXIT
+    exit 1
+}
+
+trap "stop_demo" INT QUIT TERM EXIT
+
+start_experiment && check_ssh_available && start_border_router && start_demo_node
