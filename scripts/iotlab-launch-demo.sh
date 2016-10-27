24d77246543a07cf17a3cf210a50f925e0709352
Alexandre Abadie <alexandre.abadie@inria.fr>
2016-10-27 10:05:51 +0200

    scripts: initial version of the iot-lab automatic demo startup

---
+++ scripts/iotlab-launch-demo.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+BR_FW = $1
+DEMO_FW = $2
+
+# Submit an experiment in Saclay on nodes A8 144 and 145
+# - node-a8-144 will be used as a border router
+# - node-a8-145 will run the dashboard enable firmware. This node is visible at
+# http://demo-fit.saclay.inria.fr
+experiment-cli submit -d 120 -l saclay,a8,144+145
+experiment-cli wait
+
+# Flash the firmwares on the nodes using ssh-cli-tools
+# 1. Flash the border router
+open-a8-cli update-m3 $(BR_FW) -l saclay,a8,144
+
+# Start the border router
+ssh node-a8-144 'cd ~/A8/riot/RIOT/dist/tools/ethos&& screen -S br ./start_network.sh /dev/ttyA8_M3 tap0 2001:0660:3207:490::/64'
+
+# 2. Flash the demo node
+open-a8-cli update-m3 $(DEMO_FW) -l saclay,a8,145
+
+
