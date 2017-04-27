36a972453a0c53830590c9206b272c49210e18dd
Alexandre Abadie <alexandre.abadie@inria.fr>
2017-04-27 18:09:19 +0200

    broker: deep application refactoring

--- pyaiot/broker/application.py
+++ pyaiot/broker/application.py
@@ -30,40 +30,13 @@
 """Broker tornado application module."""
 
 import json
-import tornado
 import logging
 import uuid
-
+from tornado.ioloop import PeriodicCallback
 from tornado import gen, web, websocket
 
-from .coap import _forward_data_to_node, _discover_node
-from .data import coap_nodes, client_sockets, node_sockets
+from .coap import CoapController
 from .logger import logger
-from .utils import _broadcast_message
-
-
-class BrokerPostHandler(web.RequestHandler):
-    @tornado.web.asynchronous
-    def get(self, path=None):
-        pass
-
-    @tornado.web.asynchronous
-    @gen.coroutine
-    def post(self):
-        """Forward POST request to a CoAP PUT request."""
-        message = self.request.body.decode('utf-8')
-        try:
-            data = json.loads(message)
-        except TypeError as e:
-            logger.warning(e)
-            return "{}".format(e)
-        except json.JSONDecodeError:
-            reason = ("Invalid message received "
-                      "'{}'. Only JSON format is supported.".format(message))
-            logger.warning(reason)
-            return reason
-
-        _forward_data_to_node(data['data'])
 
 
 class BrokerWebsocketHandler(websocket.WebSocketHandler):
@@ -77,92 +50,114 @@ class BrokerWebsocketHandler(websocket.WebSocketHandler):
         logger.debug("New websocket opened")
 
     @gen.coroutine
-    def on_message(self, message):
+    def on_message(self, raw):
         """Triggered when a message is received from the web client."""
         try:
-            data = json.loads(message)
+            message = json.loads(raw)
         except TypeError as e:
             logger.warning(e)
             self.close(code=1003,
-                       reason="Invalid message '{}'.".format(message))
+                       reason="Invalid message '{}'.".format(raw))
             return
         except json.JSONDecodeError:
             reason = ("Invalid message received "
-                      "'{}'. Only JSON format is supported.".format(message))
+                      "'{}'. Only JSON format is supported.".format(raw))
             logger.warning(reason)
             self.close(code=1003, reason="{}.".format(reason))
             return
 
-        if 'type' not in data and 'data' not in data:
-            self.close(code=1003,
-                       reason="Invalid message '{}'.".format(message))
-
-        if data['type'] == "new":
-            if data['data'] == "client":
-                logger.debug("new client connected")
-                client_sockets.append(self)
-                for node in coap_nodes():
-                    self.write_message(json.dumps({'command': 'new',
-                                                   'node': node.address}))
-                    _discover_node(node, self)
-                for ws, uid in node_sockets.items():
-                    self.write_message(json.dumps({'command': 'new',
-                                                   'node': uid}))
-                    ws.write_message(json.dumps({'request': 'discover'}))
-
-            elif data['data'] == "node":
-                logger.debug("new node from websocket")
-                node_sockets.update({self: str(uuid.uuid4())})
-                _broadcast_message(json.dumps({'command': 'new',
-                                               'node': node_sockets[self]}))
-
-        elif data['type'] == "update":
-            if self in client_sockets:
-                logger.debug("new update from client websocket")
-                _forward_data_to_node(data['data'], origin="Websocket")
-            elif self in node_sockets:
-                logger.debug("new update from node websocket")
-                for key, value in data['data'].items():
-                    _broadcast_message(
-                        json.dumps({'command': 'update',
-                                    'node': node_sockets[self],
-                                    'endpoint': '/' + key,
-                                    'data': value}))
-        else:
-            self.close(code=1003,
-                       reason="Unknown message type '{}'.".format(
-                              data['type']))
+        self.application.handle_ws_message(self, message)
 
     def on_close(self):
         """Remove websocket from internal list."""
-
         logger.debug("Websocket closed")
-        if self in client_sockets:
-            client_sockets.remove(self)
-        elif self in node_sockets:
-            _broadcast_message(json.dumps({'node': node_sockets[self],
-                                           'command': 'out'}))
-            node_sockets.pop(self)
+        self.application.remove_ws(self)
 
 
 class BrokerApplication(web.Application):
     """Tornado based web application providing live nodes on a network."""
 
     def __init__(self, options=None):
-        self.options = options
-        self._nodes = {}
+        self._options = options
+        self._coap_controller = CoapController(self, max_time=options.max_time)
+        self.client_sockets = []
+        self.node_sockets = {}
         self._log = logger
-        if self.options is not None and self.options.debug:
-            self._log.setLevel(logging.DEBUG)
+        if self._options is not None and self._options.debug:
+            logger.setLevel(logging.DEBUG)
 
         handlers = [
-            (r"/post", BrokerPostHandler),
             (r"/ws", BrokerWebsocketHandler),
         ]
         settings = {'debug': True,
                     'cookie_secret': 'MY_COOKIE_ID',
                     'xsrf_cookies': False}
+
+        PeriodicCallback(self._coap_controller.check_dead_nodes, 1000).start()
+
         super().__init__(handlers, **settings)
-        if self.options is not None:
-            self._log.info('Application started, listening on port {0}'
-                           .format(self.options.port))
+        if self._options is not None:
+            logger.info('Application started, listening on port {}'
+                        .format(self._options.port))
+
+    def broadcast_to_clients(self, message):
+        """Broadcast message to all opened websockets clients."""
+        logger.debug("Broadcasting message '{}' to web clients."
+                     .format(message))
+        for ws in self.client_sockets:
+            ws.write_message(message)
+
+    def handle_ws_message(self, ws, message):
+        """Handle a message received from a websocket."""
+        logger.debug("Handling message '{}' received from websocket."
+                     .format(message))
+
+        if 'type' not in message and 'data' not in message:
+            ws.close(code=1003, reason="Invalid message '{}'.".format(message))
+
+        if message['type'] == "new":
+            if message['data'] == "client":
+                logger.debug("new client connected")
+                self.client_sockets.append(ws)
+                for node in self._coap_controller.nodes:
+                    ws.write_message(json.dumps({'command': 'new',
+                                                 'node': node.address}))
+                    self._coap_controller.discover_node(node, ws=ws)
+                for node_ws, uid in self.node_sockets.items():
+                    ws.write_message(json.dumps({'command': 'new',
+                                                 'node': uid}))
+                    node_ws.write_message(json.dumps({'request': 'discover'}))
+
+            elif message['data'] == "node":
+                logger.debug("new node from websocket")
+                self.node_sockets.update({ws: str(uuid.uuid4())})
+                self.broadcast_to_clients(
+                    json.dumps({'command': 'new',
+                                'node': self.node_sockets[ws]}))
+
+        elif message['type'] == "update":
+            if ws in self.client_sockets:
+                logger.debug("new update from client websocket")
+                self._coap_controller.send_data_to_node(message['data'],
+                                                        origin="Websocket")
+            elif ws in self.node_sockets:
+                logger.debug("new update from node websocket")
+                for key, value in message['data'].items():
+                    self.broadcast_to_clients(
+                        json.dumps({'command': 'update',
+                                    'node': self.node_sockets[ws],
+                                    'endpoint': '/' + key,
+                                    'data': value}))
+        else:
+            ws.close(code=1003, reason="Unknown message type '{}'."
+                     .format(message['type']))
+
+    def remove_ws(self, ws):
+        """Remove websocket that has been closed."""
+        if ws in self.client_sockets:
+            self.client_sockets.remove(ws)
+        elif ws in self.node_sockets:
+            self.broadcast_to_clients(
+                json.dumps({'node': self.node_sockets[ws],
+                            'command': 'out'}))
+            self.node_sockets.pop(ws)
