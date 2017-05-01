dd8e8f9d6689114f331a16752bccdac53e1b34eb
Alexandre Abadie <alexandre.abadie@inria.fr>
2017-05-01 11:05:10 +0200

    utils/ws: small improvements

--- utils/ws/ws_test_client.py
+++ utils/ws/ws_test_client.py
@@ -1,22 +1,43 @@
 """One shot Websocket test client."""
 
+import sys
 import argparse
 import json
 import websocket
 
+from pyaiot.common.messaging import Message
 
-def main(args):
-    """Main function."""
-    ws = websocket.create_connection("ws://{}:{}/node".format(args.host,
-                                                              args.port))
-    ws.send(json.dumps({'type': 'new',
-                        'data': 'node'}))
+
+def init_node(ws):
+    """Send initial node information"""
     ws.send(json.dumps({'type': 'update',
                         'data': {'node': 'fd00:aaaa:bbbb::1',
                                  'name': 'websocket',
                                  'led': '0',
                                  'os': 'riot'}}))
-    print(ws.recv())
+
+
+def main(args):
+    """Main function."""
+    try:
+        ws = websocket.create_connection("ws://{}:{}/node".format(args.host,
+                                                                  args.port))
+    except ConnectionRefusedError:
+        print("Cannot connect to ws://{}:{}".format(args.host, args.port))
+        return
+
+    ws.send(json.dumps({'type': 'new', 'data': 'node'}))
+    init_node(ws)
+    while True:
+        try:
+            msg = ws.recv()
+        except:
+            print("Connection closed")
+            break
+        else:
+            print(msg)
+            if msg == Message.discover_node():
+                init_node(ws)
 
 
 if __name__ == '__main__':
@@ -26,4 +47,8 @@ if __name__ == '__main__':
     parser.add_argument('--port', type=str, default="8001",
                         help="CoGateway port")
     args = parser.parse_args()
-    main(args)
+    try:
+        main(args)
+    except KeyboardInterrupt:
+        print("Exiting")
+        sys.exit()
