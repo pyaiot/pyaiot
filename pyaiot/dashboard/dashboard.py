5a3327a3426f43092da9bc32f8405f86e3dfbc36
Alexandre Abadie <alexandre.abadie@inria.fr>
2019-03-05 10:23:21 +0100

    dashboard: asynchronous decorator doesn't exist anymore in tornado 6

--- pyaiot/dashboard/dashboard.py
+++ pyaiot/dashboard/dashboard.py
@@ -45,7 +45,7 @@ logger = logging.getLogger("pyaiot.dashboard")
 
 
 class DashboardHandler(web.RequestHandler):
-    @tornado.web.asynchronous
+
     def get(self, path=None):
         self.render("dashboard.html",
                     wsproto="wss" if options.broker_ssl else "ws",
