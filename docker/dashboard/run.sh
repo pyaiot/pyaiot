e19d4b23a80daa39d6f8a74d8b2005fb52e6f732
Francisco Molina <femolina@uc.cl>
2021-01-29 10:46:23 +0100

    docker/dashboard: add web port as parameter

--- docker/dashboard/run.sh
+++ docker/dashboard/run.sh
@@ -2,9 +2,11 @@
 
 : ${BROKER_HOST:=localhost}
 : ${BROKER_PORT:=8000}
+: ${WEB_PORT:=8080}
 : ${MAP_API_KEY}:=invalid}
 
 aiot-dashboard --broker-host=${BROKER_HOST} --broker-port=${BROKER_PORT} \
+                --web-port=${WEB_PORT} \
                 --map-api-key=${MAP_API_KEY} \
                 --static-path=/opt/pyaiot/pyaiot/dashboard/static \
                 --debug
