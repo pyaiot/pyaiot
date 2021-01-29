fd6ab733a2df596bd55198af7b1df4092e3fba18
Francisco Molina <femolina@uc.cl>
2021-01-29 10:46:03 +0100

    docker/broker: add host and port as parameter

--- docker/broker/run.sh
+++ docker/broker/run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-aiot-broker --debug
+: ${BROKER_HOST:=pyaiot-broker}
+: ${BROKER_PORT:=8000}
+
+aiot-broker --broker-host=${BROKER_HOST} --broker-port=${BROKER_PORT} \
+            --debug
