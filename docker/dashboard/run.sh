38dacd8a8b9a9a75ca17f0be1f41136deba8b633
Francisco Molina <femolina@uc.cl>
2021-02-23 09:16:15 +0100

    docker/dashboard/run.sh: add BROKER_SSL argument

--- docker/dashboard/run.sh
+++ docker/dashboard/run.sh
@@ -2,11 +2,12 @@
 
 : ${BROKER_HOST:=localhost}
 : ${BROKER_PORT:=8000}
+: ${BROKER_SSL}:=False}
 : ${WEB_PORT:=8080}
 : ${MAP_API_KEY}:=invalid}
 
 aiot-dashboard --broker-host=${BROKER_HOST} --broker-port=${BROKER_PORT} \
-                --web-port=${WEB_PORT} \
+                --web-port=${WEB_PORT} --broker-ssl=${BROKER_SSL}\
                 --map-api-key=${MAP_API_KEY} \
                 --static-path=/opt/pyaiot/pyaiot/dashboard/static \
                 --debug
