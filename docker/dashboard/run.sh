74b31ec761b02efc2a9d4a4d8cea2d89f50325dd
Francisco Molina <femolina@uc.cl>
2021-03-19 19:04:24 +0100

    docker: fix static files location when building local

--- docker/dashboard/run.sh
+++ docker/dashboard/run.sh
@@ -6,9 +6,10 @@
 : ${WEB_PORT:=8080}
 : ${MAP_API_KEY}:=invalid}
 : ${KEY_FILE}:=}
+: ${STATIC_PATH}:=/opt/pyaiot/pyaiot/dashboard/static}
 
 aiot-dashboard --broker-host=${BROKER_HOST} --broker-port=${BROKER_PORT} \
                 --web-port=${WEB_PORT} --broker-ssl=${BROKER_SSL} \
                 --map-api-key=${MAP_API_KEY} --key-file=${KEY_FILE} \
-                --static-path=/opt/pyaiot/pyaiot/dashboard/static \
+                --static-path=${STATIC_PATH} \
                 --debug
