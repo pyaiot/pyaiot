b788cf29e43ae001feb60900c7330dc7ccd8f82b
Francisco Molina <femolina@uc.cl>
2021-03-19 19:58:25 +0100

    docker: fix wrong default shell values

--- docker/coap-gw/run.sh
+++ docker/coap-gw/run.sh
@@ -2,7 +2,9 @@
 
 : ${BROKER_HOST:=pyaiot-broker}
 : ${BROKER_PORT:=8000}
-: ${KEY_FILE}:=}
+: ${KEY_FILE:=}
+: ${COAP_PORT:=5683}
+: ${KEY_FILE:=}
 
 aiot-coap-gateway --broker-host=${BROKER_HOST} --broker-port=${BROKER_PORT} \
-                    --key-file=${KEY_FILE}   --debug
+                  --key-file=${KEY_FILE} --coap-port=${COAP_PORT} --debug
--- docker/dashboard/run.sh
+++ docker/dashboard/run.sh
@@ -2,14 +2,14 @@
 
 : ${BROKER_HOST:=localhost}
 : ${BROKER_PORT:=8000}
-: ${BROKER_SSL}:=False}
+: ${BROKER_SSL:=False}
 : ${WEB_PORT:=8080}
-: ${MAP_API_KEY}:=invalid}
-: ${KEY_FILE}:=}
-: ${STATIC_PATH}:=/opt/pyaiot/pyaiot/dashboard/static}
+: ${MAP_API_KEY:=invalid}
+: ${KEY_FILE:=}
+: ${STATIC_PATH:="/opt/pyaiot/pyaiot/dashboard/static"}
 
 aiot-dashboard --broker-host=${BROKER_HOST} --broker-port=${BROKER_PORT} \
                 --web-port=${WEB_PORT} --broker-ssl=${BROKER_SSL} \
                 --map-api-key=${MAP_API_KEY} --key-file=${KEY_FILE} \
-                --static-path=${STATIC_PATH} \
+                --static-path=/opt/pyaiot/pyaiot/dashboard/static \
                 --debug
