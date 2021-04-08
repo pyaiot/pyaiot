3286d82ea53a16062dfe11178cc9baa1996fb155
Francisco Molina <femolina@uc.cl>
2021-04-08 16:52:37 +0200

    docker/dashboard: add more optional parameters

--- docker/dashboard/run.sh
+++ docker/dashboard/run.sh
@@ -7,9 +7,15 @@
 : ${MAP_API_KEY:=invalid}
 : ${KEY_FILE:=}
 : ${STATIC_PATH:="/opt/pyaiot/pyaiot/dashboard/static"}
+: ${CAMERA_URL:=}
+: ${DASHBOARD_TITLE:="IoT Dashboard"}
+: ${DASHBOARD_LOGO:="/static/assets/logo-riot.png"}
+: ${DASHBOARD_FAVICON:="/static/assets/favicon192.png"}
 
 aiot-dashboard --broker-host=${BROKER_HOST} --broker-port=${BROKER_PORT} \
                 --web-port=${WEB_PORT} --broker-ssl=${BROKER_SSL} \
                 --map-api-key=${MAP_API_KEY} --key-file=${KEY_FILE} \
                 --static-path=/opt/pyaiot/pyaiot/dashboard/static \
+                --camera-url=${CAMERA_URL} --title="${DASHBOARD_TITLE}" \
+                --logo=${DASHBOARD_LOGO} --favicon=${DASHBOARD_FAVICON} \
                 --debug
